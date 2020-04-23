c49ab4858a069e792e431e36380d33a5cc91a2aa
Hemil Ruparel <hemilruparel2002@gmail.com>
2020-04-23 11:16:22 +0530

    refactored

--- main.cpp
+++ main.cpp
@@ -1,87 +1,51 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
+#include <random>
+
 #include"que.h"
 
-using namespace std;
+int randomNumber(int from, int to) {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(from, to);
+
+    return dist6(rng);
+}
+
+std::vector<Question> getQuestions() {
+    return {
+        Question("Question 1", {"Option 1", "Option 2", "Option3"}, 2),
+        Question("Question 2", {"Option 1", "Option 2", "Option3"}, 1),
+        Question("Question 3", {"Option 1", "Option 2", "Option3"}, 3),
+    };
+}
+
+void removeQuestion(std::vector<Question> &questions, int index) {
+    questions.erase(questions.begin() + index);
+}
 
 int main()
 {
-    int ans=0,counter=0,i=0;
+    int correctAnswers = 0;
 
-    srand(time(NULL));
-    for (i=0;i<=10;i++)
-    {
-    int rand_fun =(rand()% 20 );
-    switch(rand_fun)
-    {
-    case 1:
-        counter=que1(counter,ans);
-        break;
-         case 2:
-        counter=que2(counter,ans);
-        break;
-         case 3:
-        counter=que3(counter,ans);
-        break;
-         case 4:
-        counter=que4(counter,ans);
-        break;
-         case 5:
-        counter=que5(counter,ans);
-        break;
-         case 6:
-        counter=que6(counter,ans);
-        break;
-         case 7:
-        counter=que7(counter,ans);
-        break;
-         case 8:
-        counter=que8(counter,ans);
-        break;
-         case 9:
-        counter=que9(counter,ans);
-        break;
-         case 10:
-        counter=que10(counter,ans);
-        break;
-         case 11:
-        counter=que11(counter,ans);
-        break;
-         case 12:
-        counter=que12(counter,ans);
-        break;
-         case 13:
-        counter=que13(counter,ans);
-        break;
-         case 14:
-        counter=que14(counter,ans);
-        break;
-         case 15:
-        counter=que15(counter,ans);
-        break;
-         case 16:
-        counter=que16(counter,ans);
-        break;
-         case 17:
-        counter=que17(counter,ans);
-        break;
-         case 18:
-        counter=que18(counter,ans);
-        break;
-         case 19:
-        counter=que19(counter,ans);
-        break;
-         case 20:
-        counter=que20(counter,ans);
-        break;
-         default:
-            break;
+    std::vector<Question> questionsLeft = getQuestions();
+    
+    while (questionsLeft.size() != 0) {
+        int random = randomNumber(0, questionsLeft.size() - 1);
 
+        Question question = questionsLeft.at(random);
+
+        question.display();
+
+        if (question.isCorrect()) {
+            correctAnswers++;
+
+            std::cout << "\nCorrect Answer!!!\n";
+        } else {
+            std::cout << "\nWrong Answer :(\n";
         }
+
+        removeQuestion(questionsLeft, random);
     }
-
-    return 0;
-
-
+    
+    std::cout << correctAnswers << " correctAnswers\n";
 }
--- que.h
+++ que.h
@@ -1,268 +1,34 @@
 #pragma once
 
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <array>
 
-using namespace std;
+const int numOfOptions = 3;
 
-int que1(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 1" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-       if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
+struct Question {
+    std::string question;
+    std::array<std::string, numOfOptions> options;
+    int correctOption, chosenOption;
 
+    Question(std::string question, std::array<std::string, numOfOptions> options, int correctOption) {
+        this->question = question;
+        this->options = options;
+        this->correctOption = correctOption;
     }
- int que2(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 2" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }
-        return counter;
 
-    }
-    int que3(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 3" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
+    void display() {
+        std::cout << "\n\n" << question << "\n";
 
-    }
-    int que4(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 4" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
+        int counter = 1;
+        for (auto option : options) {
+            std::cout << counter << ". " << option << "\n";
+            counter++;
+        }
 
+        std::cin >> chosenOption;
     }
-    int que5(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 5" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
 
+    bool isCorrect() {
+        return chosenOption == correctOption;
     }
-    int que6(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 6" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que7(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 7" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que8(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 8" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que9(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 9" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que10(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 10" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que11(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 11" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que12(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 12" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que13(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 13" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-     int que14(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 14" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que15(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 15" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que16(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 16" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que17(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 17" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que18(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 18" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que19(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 19" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
-    int que20(int counter,int ans)
-    {
-       cout << "counter is " << counter;
-       cout << "This is Que 20" << endl;
-       cout << "1.Op1     2.Op2\n3.Op3     4.Op4 \n";
-       cin >> ans;
-       system("cls");
-         if(ans==1)
-    {
-        counter = counter+1;
-    }    return counter;
-
-    }
+};
